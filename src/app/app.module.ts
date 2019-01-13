130511131d06a0b6c043e1b8e5289420b8dece59
Wilmer A. Rubio Rojas <rubiorojasw@gmail.com>
2019-01-13 15:13:29 -0500

    Home component and routes module was created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Routes
+import { APP_ROUTING } from './app.routes';
+
+// Components
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { CarBrandsComponent } from './components/shared/car-brands/car-brands.component';
 import { CarInputEventComponent } from './components/shared/car-input-event/car-input-event.component';
+import { HomeComponent } from './components/home/home.component';
+import { from } from 'rxjs';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     CarBrandsComponent,
-    CarInputEventComponent
+    CarInputEventComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    APP_ROUTING
   ],
   providers: [],
   bootstrap: [AppComponent]
