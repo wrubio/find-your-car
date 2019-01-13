cb6ae1a3b566cf884795076d609eb06f198fb779
Wilmer A. Rubio Rojas <rubiorojasw@gmail.com>
2019-01-13 11:38:12 -0500

    Adding Bootstrap 4.1.3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
+import { NavbarComponent } from './components/shered/navbar/navbar.component';
 
 @NgModule({
   declarations: [
