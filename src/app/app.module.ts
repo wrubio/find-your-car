6ba69cb5662b7c40342b8362dd19e496bce5a05e
Wilmer A. Rubio Rojas <rubiorojasw@gmail.com>
2019-01-13 14:42:48 -0500

    The layout of the inputs was developed to search and compare cars and the car-input-event component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/shered/navbar/navbar.component';
-import { CarBrandsComponent } from './components/shered/car-brands/car-brands.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { CarBrandsComponent } from './components/shared/car-brands/car-brands.component';
+import { CarInputEventComponent } from './components/shared/car-input-event/car-input-event.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    CarBrandsComponent
+    CarBrandsComponent,
+    CarInputEventComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/shared/car-input-event/car-input-event.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-car-input-event',
+  templateUrl: './car-input-event.component.html',
+  styleUrls: ['./car-input-event.component.css']
+})
+export class CarInputEventComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
